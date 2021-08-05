73e64325e7dc63cca8c9b9bb3fb540a455246b51
Taryn Hill <taryn@phrohdoh.com>
2021-08-05 12:00:04 -0500

    wip: attempt to execute clojure received from network
    crashes once the bevy app is closed, see following logs
    
    [src/lib.rs:40] find_my_ip_address() = Some(192.168.1.456)
    [src/lib.rs:41] ip_addr = 192.168.1.456
    [src/lib.rs:43] ip_addr = 127.0.0.1
    [src/lib.rs:45] server_addr = 127.0.0.1:8080
    thread 'IO Task Pool (1)' panicked at 'failed to join thread: Resource deadlock avoided (os error 11)', library/std/src/sys/unix/thread.rs:208:13
    stack backtrace:
       0:        0x10d7c32f4 - <std::sys_common::backtrace::_print::DisplayBacktrace as core::fmt::Display>::fmt::hbf0f7aeb2a01393a
       1:        0x10d827bec - core::fmt::write::h8160330c41daaf61
       2:        0x10d7b51da - std::io::Write::write_fmt::haa5623a2a8d2ec9f
       3:        0x10d7c6ebf - std::panicking::default_hook::{{closure}}::h5fe994d86d862da0
       4:        0x10d7c69bd - std::panicking::default_hook::hba89cfe1e23145fb
       5:        0x10d7c76f0 - std::panicking::rust_panic_with_hook::h674e3a191f056728
       6:        0x10d7c7195 - std::panicking::begin_panic_handler::{{closure}}::hc97521b9fa6c7ab0
       7:        0x10d7c3798 - std::sys_common::backtrace::__rust_end_short_backtrace::h9a967faa138ad029
       8:        0x10d7c70fa - _rust_begin_unwind
       9:        0x10d851eab - std::panicking::begin_panic_fmt::h4d9d4e08b31fe5f2
      10:        0x10d7d2c19 - std::sys::unix::thread::Thread::join::h7dca6cef645f1fd7

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod scripting;
 mod units;
 
-use std::env;
+use std::{env, net::SocketAddr, sync::Arc};
 use bevy::prelude::*;
+use bevy_networking_turbulence::{NetworkEvent, NetworkResource, NetworkingPlugin, find_my_ip_address};
+use cljrs::{environment::Environment as Env, repl::Repl};
 use scripting::{
     ScriptableApp as _,
     ScriptingConfig,
@@ -22,6 +24,39 @@ pub fn build_app() -> AppBuilder {
        .add_plugin(units::UnitPlugin)
        .add_scripting(create_custom_cljrs_env(), ScriptingConfig {
            startup_repl: env::args().nth(1) == Some("--with-startup-repl".to_owned()),
-       });
+       })
+       // networking lib
+       .add_plugin(NetworkingPlugin::default())
+       .add_startup_system(sys_setup_net_listen.system())
+       // our networking
+       .add_system(sys_on_packet_recv.system())
+    ;
     app
+}
+
+fn sys_setup_net_listen(
+    mut net: ResMut<NetworkResource>,
+) {
+    let ip_addr = dbg!(find_my_ip_address()).expect("can't get own IP");
+    let ip_addr = dbg!(ip_addr);
+    let ip_addr = std::net::IpAddr::V4(std::net::Ipv4Addr::LOCALHOST);
+    let ip_addr = dbg!(ip_addr);
+    let server_addr = SocketAddr::new(ip_addr, 8080);
+    let server_addr = dbg!(server_addr);
+    net.listen(server_addr, None, None);
+}
+
+fn sys_on_packet_recv(
+    mut net: ResMut<NetworkResource>,
+    mut rdr: EventReader<NetworkEvent>,
+    mut arc_env: Res<Arc<Env>>,
+) {
+    let repl = Repl::new(arc_env.clone());
+
+    for evt in rdr.iter() {
+        if let NetworkEvent::Packet(hndl, bytes) = evt {
+            let content = String::from_utf8_lossy(bytes);
+            repl.eval_readable(&mut content.as_bytes());
+        }
+    }
 }
